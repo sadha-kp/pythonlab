64611008726f6e18965085efc4ed569ae89de2b7
USER <sadhakp2003@gmail.comgit config --global user.name sadha-kp>
2025-09-17 12:35:50 +0530

    update

--- arp.py
+++ arp.py
@@ -1,16 +1,9 @@
-a=int(input("enter the first number: "))
-b=int(input("enter the second number: "))
-sum=a+b
-sub=a-b
-mul=a*b
-div+a/b
-mod=a%b
-intdiv=a//b
-print(f'ARTHMATIC OPERATION OF THE NUMBER {a} and {b}  is \n')
-print(f'sum is {sum} \n') 
-print(f'substraction is {sub} \n')
-print(f'multiplication is {mul} \n')
-print(f'division is {div} \n')
-print(f'modulus is {mod} \n')
-print(f'intdivision is {int div} \n')
-
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+print("addittion:", a + b)
+print("substraction:", a-b)
+print("multiplication:", a*b)
+if b !=0:
+    print("division:", a / b)
+else:
+    print("division: cannot divide by zero")
