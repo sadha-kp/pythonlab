c31d66155e0f46f7ca1a4a7d86d17d91e12b3558
USER <sadhakp2003@gmail.comgit config --global user.name sadha-kp>
2025-09-17 11:59:22 +0530

    done

---
+++ biggest.py
@@ -0,0 +1,4 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+print("biggest number is:",max(a,b,c))
---
+++ integer.py
@@ -0,0 +1,2 @@
+n=int(input("enter an integer:"))
+print(n+int(str(n)*2)+int(str(n)*3))
