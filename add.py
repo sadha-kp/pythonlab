73a4055ae1619861764b2a98fa82d651cd86e0a4
sadha-kp <sadhakp2003@gmail.com>
2025-09-19 10:33:51 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-num1=float(input("enter first number:"))
+num1=float(input("enter first number :"))
 num2=float(input("enter second number:"))
 sum_result=num1+num2
 print("the sum of",num1,"and",num2,"is",sum_result)            
