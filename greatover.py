52011e5e37c616487a6167317c310d76b3ad6b27
sadha-kp <sadhakp2003@gmail.com>
2025-09-23 10:58:28 +0530

    Update greatover.py

--- greatover.py
+++ greatover.py
@@ -1,3 +1,3 @@
-num = input("enter integers separated by spaces:").split()
+num = input("enter integers  separated by spaces:").split()
 result = [ int(n) if int(n)<=100 else "over" for n in num]
 print (result)
