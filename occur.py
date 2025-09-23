0b2d99275f55baeab4febc96b33b76ce62a34d8b
sadha-kp <sadhakp2003@gmail.com>
2025-09-23 10:59:16 +0530

    Update occur.py

--- occur.py
+++ occur.py
@@ -1,3 +1,3 @@
-names = input("Enter first names separated by commas: ").split(',')
+names = input("Enter first names  separated by commas: ").split(',')
 count = sum(name.lower().count('a') for name in names)
 print("Occurrences of 'a':", count)
