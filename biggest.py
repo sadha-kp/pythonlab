7cc1bfbfc9416dac122b39b4b330e1f3d419a425
sadha-kp <sadhakp2003@gmail.com>
2025-09-23 10:56:35 +0530

    Update biggest.py

--- biggest.py
+++ biggest.py
@@ -1,4 +1,4 @@
 a=int(input("enter first number:"))
 b=int(input("enter second number:"))
-c=int(input("enter third number:"))
+c=int(input("enter  third number:"))
 print("biggest number is:",max(a,b,c))
