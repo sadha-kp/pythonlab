cd519b0d9c2243baefaeea70e1a9a2f4a9b042e1
sadha-kp <sadhakp2003@gmail.com>
2025-11-06 10:19:03 +0530

    done

---
+++ letter.py
@@ -0,0 +1,14 @@
+def compare(s1, s2, n):
+    return s1[:n] == s2[:n]
+
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+n = int(input("Enter number of characters to compare: "))
+
+print("Result:", compare(s1, s2, n))
+
+~                                                                                                                                                             
+~                                                                                                                                                             
+~                                                                                                                                                             
+~                                                                                                                                                             
+~   
