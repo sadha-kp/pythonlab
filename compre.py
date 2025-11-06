172b99262936dd2d3ac6bc2a6bb87c5c37e0637f
sadha-kp <sadhakp2003@gmail.com>
2025-11-06 10:15:16 +0530

    Create compre.py

---
+++ compre.py
@@ -0,0 +1,10 @@
+print([int(n) for n in input("numbers: ").split() if int(n) > 0])
+
+print([int(n) ** 2 for n in input("numbers to square: ").split()])
+
+print([ch for ch in input("word: ") if ch in 'aeiouAEIOU'])
+
+print([ord(ch) for ch in input("another word: ")])
+
+
+~                                                                                                                                                            
