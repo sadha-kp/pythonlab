614423122399384358c83afcc6e4f6b0849909a9
sadha-kp <sadhakp2003@gmail.com>
2025-11-06 10:19:57 +0530

    Create odd.py

---
+++ odd.py
@@ -0,0 +1,11 @@
+def check(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+n = int(input("Enter a number: "))
+print("The number is", check(n))
+
+~                                                                                                                                                             
+~ 
