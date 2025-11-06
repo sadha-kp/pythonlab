0644a8052cb6a3983ff16d26b2d3c360b5100038
sadha-kp <sadhakp2003@gmail.com>
2025-11-06 10:16:10 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+um = int(input("enter a number:"))
+fact = 1
+for i in range (1,num+1):
+    fact = fact*i
+print("the factorial of",num,"is",fact)
+~                                                                             
+~                                                                             
+~           
