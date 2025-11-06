b77049ac71bdeda5f9b0892b84f7afccaf081764
sadha-kp <sadhakp2003@gmail.com>
2025-11-06 10:12:06 +0530

    done

---
+++ perfect.py
@@ -0,0 +1,11 @@
+low = int(input("Enter lower range: "))
+high = int(input("Enter upper range: "))
+
+for n in range(low, high + 1):
+    if int(n ** 0.5) ** 2 == n:
+        if all(int(d) % 2 == 0 for d in str(n)):
+            print(n)
+
+~                                                                             
+~                                                                             
+~             
