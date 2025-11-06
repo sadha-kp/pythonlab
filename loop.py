71d306315a15fffcae722e55cfa1587fab9e859c
sadha-kp <sadhakp2003@gmail.com>
2025-11-06 10:11:00 +0530

    done

---
+++ loop.py
@@ -0,0 +1,16 @@
+n = int(input("Enter number of elements: "))
+numbers = []
+
+for i in range(n):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+
+total = 0
+for num in numbers:
+    total += num
+
+print("Sum of all items:", total)
+
+~                                                                             
+~                                                                             
+~        
